72d410b2115fd0e0404bfecc5cc62b881737b4dd
vishesh <visheshshetty86@gmail.com>
2026-05-13 11:44:46 +0530

    add landing page layout and dashboard page

--- src/Landing page/Landing page layout/landingPageLayout.jsx
+++ src/Landing page/Landing page layout/landingPageLayout.jsx
@@ -1,14 +1,13 @@
 import SideMenuLayout from "../../sidemenu/SideMenuLayout/SideMenuLayout";
+import DashboardPage from "../../Pages/DashboardPage";
 
 export default function LandingPageLayout() {
  return (
    <div className="flex h-screen">
       <SideMenuLayout />
 
-      <div className="flex-1 bg-[#111114] flex items-center justify-center">
-         <h1 className="text-white text-4xl michroma">
-           Welcome to Pluto Project Management
-         </h1>
+      <div className="flex-1 bg-[#111114] ">
+       <DashboardPage />
       </div>
 
    </div>
---
+++ src/Pages/DashboardPage.jsx
@@ -0,0 +1,9 @@
+export default function DashboardPage(){
+return (
+<div className="flex items-center justify-center h-full"> 
+    <h1 className="text-white text-4xl michroma">
+        Welcome to Pluto Project Management
+    </h1>
+</div>
+)
+}
