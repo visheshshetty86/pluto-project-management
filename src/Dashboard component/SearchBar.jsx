f0d6d6ffa11c54622f229ae582e5243baabb6515
vishesh <visheshshetty86@gmail.com>
2026-05-22 02:42:56 +0530

    feature search bar added

---
+++ src/Dashboard component/SearchBar.jsx
@@ -0,0 +1,14 @@
+import { Search } from 'lucide-react';
+
+
+export default function SearchBar() {
+  return (
+    
+      <div className=" text-[#616161] w-[220px] border border-gray-600  flex items-center gap-4 ">
+        <Search className='text-gray-500' w-2 h-2 />
+        <input type="text" 
+        placeholder="Search Tasks and Projects"
+        className='w-full outline-none border-none ' />
+      </div>
+  );
+}
--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -1,14 +1,19 @@
 import HeaderGreetings from "../Dashboard component/HeaderGreetings";
+import SearchBar from "../Dashboard component/SearchBar";
 
 export default function DashboardPage(){
 return (
 
 
-    <div className="ml-4">
+    <div className="ml-6 mt-6">
         <div>
             <HeaderGreetings />
         </div>
         
+
+        <div className= "mt-4 display flex justify-end mr-20">
+            <SearchBar />
+        </div>
     </div>
 )
 }
